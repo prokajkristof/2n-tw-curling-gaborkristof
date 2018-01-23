318dbf12f7fabc348dbb5ca0282be2a73c767c06
Gabor Szabo <puma678@gmail.com>
2018-01-23 10:18:02 +0100

    simulation basic logic

--- Main.java
+++ Main.java
@@ -5,9 +5,11 @@ public class Main {
         Person j3 = new Person("Jani", 25, 75, "s");
         Person j4 = new Person("Jani", 25, 75, "sa");
         Team[] teams = Data.getTeams("teams.csv");
-        System.out.println(teams[1].getRating());
-        Person[] players = Data.getPersons("players.csv");
-        Data.writeTeamstoFile("teams2.csv", teams);
-        Data.writePlayerstoFile("players2.csv", players);
+        for (Team var : teams) {
+            System.out.println(var.getName() + " rating: " + var.getRating());
+        }
+        Team[] parbaj = Simulation.getRandomTeams(teams);
+        System.out.println(parbaj[0].getName() + " vs " +parbaj[1].getName());
+        System.out.println(Simulation.getResult(parbaj[0], parbaj[1], true).getName());
     }
 }
