1ae354419801539133176846079090e7661bfc71
Gabor Szabo <puma678@gmail.com>
2018-01-24 10:41:24 +0100

    more rearrange

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,21 @@
-public class Main{
-    public static Simulation generateSimulation(int round){
-        Simulation vmi = new Simulation();
-        return vmi;
-    }
+public class Main {
     public static void main(String[] args) {
-        generateSimulation(0);
+        Simulation result = generateSimulation(Integer.parseInt(args[0]));
+        result.generateData();
+    }
+
+    public static Simulation generateSimulation(int round) {
+        Team[] teams = Data.getTeams("teams.csv");
+        Simulation result = new Simulation();
+        for (int i = 0; i < round; i++) {
+            Team[] competitors = Simulator.getRandomTeams(teams);
+            Team winner = Simulator.getWinner(competitors[0], competitors[1]);
+            Team[] data = new Team[3];
+            data[0] = competitors[0];
+            data[1] = competitors[1];
+            data[2] = winner;
+            result.add(data);
+        }
+        return result;
     }
 }
--- main/Simulation.java
+++ main/Simulation.java
@@ -1,8 +1,37 @@
 public class Simulation {
-    public void generateData() {
 
+    private Team[][] storage = new Team[0][];
+
+    public void generateData() {
+        String[] data = new String[storage.length];
+        String line;
+        int i = 0;
+        for (Team[] team : storage) {
+            line = team[0].getName() + "," +team[1].getName() + "," +team[2].getName();
+            System.out.println(line);
+            data[i] = line;
+            i++;
+        }
+        Data.writeData(data, "simulation.csv");
     }
+
     public Simulation load() {
         return this;
     }
+    
+
+    public void add(Team[] o) {
+        int num = storage.length;
+        Team[][] temp = new Team[num + 1][];
+        for (int i = 0; i < storage.length; i++) {
+            temp[i] = storage[i];
+        }
+        temp[num] = o;
+        storage = temp;
+    }
+
+    public Team[][] getStorage() {
+        return storage;
+    }
+    
 }
