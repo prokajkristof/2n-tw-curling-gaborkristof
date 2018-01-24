581f6f07249a9e1f4daf4201d3a071740fde1eb4
prokajkristof <kiscoloshun@gmail.com>
2018-01-24 09:21:23 +0100

    logger not final

--- main/Logger.java
+++ main/Logger.java
@@ -1,10 +1,28 @@
 import java.util.Date;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.File;
 
 
 public class Logger{
+    private boolean console;
+    public Logger(boolean console){
+        this.console = console;
+    }
     public void log(String type, String message){
         Date now = new Date();
-        now.getTime();
-        System.out.format("%s\n %s %s\n", now, type, message);
+        now.getDate();
+        if (console){
+            System.out.format("%s\n %s %s\n", now, type, message);
+        }else{
+            try {
+                File path = new File("logcsv.csv");
+                PrintWriter writer = new PrintWriter(path);
+                writer.format("%s\n %s %s\n", now, type, message);
+                writer.close();
+            }
+            catch (FileNotFoundException o) {};
+        }
     }
 }
